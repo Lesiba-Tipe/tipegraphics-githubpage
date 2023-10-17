a446e03538efe3c9624fd79b2b0649c08e5147fe
Lesiba Moshweu <moshweutipe@gmail.com>
2023-10-17 16:03:31 +0200

    refactor for maintainace page

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,5 @@
 //https://getbootstrap.com/docs/5.0/components/carousel/
 
-console.log("Testing");
-
-fetch('./shared/data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Data is the parsed JSON object
-    console.log(data);
-    // You can access specific values in the JSON object
-    console.log(data.key);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('Error:', error);
-  });
 
 
 
